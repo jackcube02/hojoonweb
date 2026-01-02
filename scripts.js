a7416604f49aae8e4d29ad96384d6e997dcbe023
Kwon Ho Joon <allgamess@naver.com>
2026-01-02 18:44:46 +0900

    Ver2.0 update

--- scripts.js
+++ scripts.js
@@ -1,63 +1,39 @@
 $(document).ready(function () {
   $('.container-slider').slick({
-    infinite: false,
     slidesToShow: 3,
-    slidesToScroll: 2,
+    slidesToScroll: 1,
     dots: true,
-    centerMode: true,              
-    centerPadding: '40px',         
+    centerMode: true,    
+    autoplay:true,
+    autoplaySpeed:3000,    
+    infinite:true,
+    speed:2000,
+    
+    centerMode: true,
+    centerPadding: '40px',
+    
+    pauseOnDotsHover: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    
+    
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerPadding: '20px'
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerPadding: '20px',
         }
+        },
+        {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerPadding: '20px',
+        },
       }
     ]
   });
 });
-
-// import { v2 as cloudinary } from 'cloudinary';
-
-// (async function() {
-
-//     // Configuration
-//     cloudinary.config({ 
-//         cloud_name: 'dcyuf1hqs', 
-//         api_key: '642594957495464', 
-//         api_secret: 'i6kk7MolaCwpkdrEJGWX-23kF5U' // Click 'View API Keys' above to copy your API secret
-//     });
-    
-//     // Upload an image
-//      const uploadResult = await cloudinary.uploader
-//        .upload(
-//            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-//                public_id: 'shoes',
-//            }
-//        )
-//        .catch((error) => {
-//            console.log(error);
-//        });
-    
-//     console.log(uploadResult);
-    
-//     // Optimize delivery by resizing and applying auto-format and auto-quality
-//     const optimizeUrl = cloudinary.url('shoes', {
-//         fetch_format: 'auto',
-//         quality: 'auto'
-//     });
-    
-//     console.log(optimizeUrl);
-    
-//     // Transform the image: auto-crop to square aspect_ratio
-//     const autoCropUrl = cloudinary.url('shoes', {
-//         crop: 'auto',
-//         gravity: 'auto',
-//         width: 500,
-//         height: 500,
-//     });
-    
-//     console.log(autoCropUrl);    
-// })();
